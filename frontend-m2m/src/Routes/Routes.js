d4c64f6ee6f617ad45ef330190e566bcc510e900
Hector <hectorberryg@gmail.com>
2019-10-15 12:51:28 +0200

    Router created

---
+++ frontend-m2m/src/Routes/Routes.js
@@ -0,0 +1,11 @@
+import { Router, Route } from 'react-router-dom';
+import PaginaRespuesta from '../Components/PaginaRespuesta';
+
+const router = () => (
+    <Router>
+        <Route exact path='/' component={}/> 
+        <Route exact path='/result' component={PaginaRespuesta}/> 
+    </Router>
+)
+
+export default router;
